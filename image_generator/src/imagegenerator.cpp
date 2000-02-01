77c4fabdc40aa7aa5b11218b302fa4b987a6dcb8
Bryant Pong <rcxking@gmail.com>
2000-02-01 05:12:16 +0000

    Adding Image Generator Code
    This commit adds in code to generate training images and test images for
    the color neural network.
    
    Signed-off-by: Bryant Pong <rcxking@gmail.com>

---
+++ image_generator/src/imagegenerator.cpp
@@ -0,0 +1,8 @@
+/*
+ * imagegenerator.cpp - This program generates a set of training images for
+ * the neural network to train on as well as a set of images to test the
+ * neural network on.
+ *
+ * Bryant Pong
+ *   
+ */
