c863f819e437554be5f056257a501faa3e3606bd
Bryant Pong <rcxking@gmail.com>
2016-01-01 23:48:54 -0800

    Adding code to generate image folders
    This commit checks in code that makes 2 nested folders to store training and
    testing images.
    
    Signed-off-by: Bryant Pong <rcxking@gmail.com>

--- image_generator/src/imagegenerator.cpp
+++ image_generator/src/imagegenerator.cpp
@@ -4,5 +4,36 @@
  * neural network on.
  *
  * Bryant Pong
- *   
+ * 1/1/16
  */
+
+// STL Includes:
+#include <iostream>
+#include <string>
+#include <sys/stat.h>
+#include <sys/types.h>
+
+// Constants:
+const std::string TOPLEVELIMAGES = "images"; 
+const std::string TRAININGIMAGES = "training_images";
+const std::string TESTIMAGES = "test_images";   
+
+// Function prototypes:
+void GenerateFolderStructure(); 
+
+// Function to create folders to hold training and test images:
+void GenerateFolderStructure()
+{
+	mkdir( TOPLEVELIMAGES.c_str(), 0777 );
+	mkdir( (TOPLEVELIMAGES+"/"+TRAININGIMAGES).c_str(), 0777 );
+	mkdir( (TOPLEVELIMAGES+"/"+TESTIMAGES).c_str(), 0777 );
+}
+
+// Main function:
+int main( int argc, char **argv )
+{
+	// Generate the folders to store test images:
+	GenerateFolderStructure();
+
+	return 0;
+}
