9a7b319f79dd29909960d42e5aa047065a5a8a77
Bryant Pong <rcxking@gmail.com>
2016-01-24 23:35:16 -0800

    Adding Basic Image Generation.
    This commit adds in functions to generate RGB images given the RGB values
    of the image.  Additional functions to display the images are included.
    
    Signed-off-by: Bryant Pong <rcxking@gmail.com>

--- image_generator/src/imagegenerator.cpp
+++ image_generator/src/imagegenerator.cpp
@@ -13,13 +13,24 @@
 #include <sys/stat.h>
 #include <sys/types.h>
 
+// OpenCV Headers:
+#include "opencv2/core/core.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+#include "opencv2/highgui/highgui.hpp"
+
 // Constants:
 const std::string TOPLEVELIMAGES = "images"; 
 const std::string TRAININGIMAGES = "training_images";
 const std::string TESTIMAGES = "test_images";   
 
+// Image Size:
+const int IMGWIDTH = 128;
+const int IMGHEIGHT = 128;
+
 // Function prototypes:
 void GenerateFolderStructure(); 
+cv::Mat GenerateImage( const int blue, const int green, const int red );  
+void DisplayImage( const std::string& imageName, const cv::Mat& image );   
 
 // Function to create folders to hold training and test images:
 void GenerateFolderStructure()
@@ -29,11 +40,31 @@ void GenerateFolderStructure()
 	mkdir( (TOPLEVELIMAGES+"/"+TESTIMAGES).c_str(), 0777 );
 }
 
+// Function to generate an RGB image:
+cv::Mat GenerateImage( const int blue, const int green, const int red )
+{
+	// Create a 128x128 3-channel image initialized to the rgb values
+	// Remember that OpenCV stores colors as BGR rather than RGB. 
+	return cv::Mat( IMGWIDTH, IMGHEIGHT, CV_8UC3, 
+	       cv::Scalar( blue, green, red ) );
+}
+
+// Function to display an image:
+void DisplayImage( const std::string& imageName, const cv::Mat& image )
+{
+	cv::imshow( imageName, image ); 
+	cv::waitKey( 0 );
+}
+
 // Main function:
 int main( int argc, char **argv )
 {
 	// Generate the folders to store test images:
-	GenerateFolderStructure();
+	//GenerateFolderStructure();
+
+	cv::Mat image = GenerateImage(255, 0, 0);
+
+	DisplayImage( "Blue Image", image );
 
 	return 0;
 }
